6f2a2f8b9141abaf3889cdfda90da0d1188bdb53
DevRafaelCena <rafael.cena@hotmail.com>
2020-02-24 15:13:32 -0300

    lançamento de faltas

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ function adicionarAluno(nome){
             const busca = alunosDaEscola.filter(aluno => aluno.nome == nome)
 
             if(busca.length < 1){
-                console.log( "Aluno não encontrado!!!")
+                console.log("###ERRO!!!!!###>>>")
+                console.log("Aluno não matricula na Escola")
+                console.log("<<<<###ERRO!!!!!###")
                return false
             } else { console.log("Aluno encontrado!!!") 
             return busca}
@@ -37,25 +39,50 @@ function adicionarAluno(nome){
 
         } 
         function matricularAluno(aluno, curso){
+            let matricula = 0
 
             for(let i=0;i<alunosDaEscola.length;i++){
                 if(alunosDaEscola[i].nome === aluno){
                     console.log(alunosDaEscola[i].nome)
-                    alunosDaEscola[i].cursos.push({nomeDoCurso: curso,dataMatricula: data })
-                    
+                    alunosDaEscola[i].faltas                    
                     console.log(alunosDaEscola[i])
+                    matricula = 1
+                
 
-                }
-
-              
-                   //return alunosDaEscola[prop].nome
-                   
-                }
-               
+                } 
+            }
+            if(matricula === 0){
+            console.log("###ERRO!!!!!###>>>")
+            console.log("Aluno não matricula na Escola")
+            console.log("<<<<###ERRO!!!!!###")
             }
            
         
+        }
 
+        function aplicarFalta(aluno){
+            /*
+             Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá incrementar uma falta ao aluno. Você deverá dar um feedback ao concluir a tarefa. Só poderá aplicar falta em aluno se o mesmo tiver matriculado em um curso.
+            */
+           for(let i=0;i<alunosDaEscola.length;i++){
+            if(alunosDaEscola[i].nome === aluno){
+                console.log(alunosDaEscola[i].nome)
+                alunosDaEscola[i].faltas++                   
+                console.log("O aluno "+alunosDaEscola[i].nome + ", tem " + alunosDaEscola[i].faltas + " faltas no total.")
+                matricula = 1
+            
+
+            } 
+        }
+        if(matricula === 0){
+            console.log("###ERRO!!!!!###>>>")
+            console.log("Aluno não matricula na Escola")
+            console.log("<<<<###ERRO!!!!!###")
+        }
+
+
+
+            }
           
         
  adicionarAluno("Rafael") 
@@ -63,4 +90,6 @@ function adicionarAluno(nome){
  listarAlunos()
  buscarAluno("Rafael22")
  matricularAluno("Rafael", 'Full Stack');
+ aplicarFalta("Rafael")
+ aplicarFalta("Rafael")
 
