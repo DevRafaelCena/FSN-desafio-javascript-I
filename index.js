b311d427fb57436244d4259561092bb1d88be5eb
DevRafaelCena <rafael.cena@hotmail.com>
2020-02-24 14:53:48 -0300

    função matricula 90%

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Base a ser utilizada
 const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
 
+const data = new Date()
 
 // implementação
-
+var objetos = {}
 function adicionarAluno(nome){
 
-    alunosDaEscola.push({nome})
+    alunosDaEscola.push({nome, notas: [], cursos:[], faltas:0})
 
-    return("Sucesso ao adcionar Aluno")
+    return("Sucesso ao adcionar Aluno : " + nome)
   
     /*Essa função irá receber uma *string* que é nome do aluno a ser criado. 
     E seguindo o modelo de aluno, o mesmo deverá ser inserido na lista de alunos.
@@ -19,8 +20,9 @@ function adicionarAluno(nome){
       
       for (variavel of alunosDaEscola) {
         console.log(variavel.nome)
-        
+                       
       }
+      
         }
         function buscarAluno(nome){
             /* Por meio dessa função, podemos pesquisar um aluno por nome na lista de aluno. Ela deverá exibir um feedback, tanto para quando encontrar o aluno, tanto quando não encontrar. E deverá devolver um aluno em seu retorno. */
@@ -28,14 +30,37 @@ function adicionarAluno(nome){
 
             if(busca.length < 1){
                 console.log( "Aluno não encontrado!!!")
+               return false
             } else { console.log("Aluno encontrado!!!") 
             return busca}
            
 
-        }     
+        } 
+        function matricularAluno(aluno, curso){
 
+            for(let i=0;i<alunosDaEscola.length;i++){
+                if(alunosDaEscola[i].nome === aluno){
+                    console.log(alunosDaEscola[i].nome)
+                    alunosDaEscola[i].cursos.push({nomeDoCurso: curso,dataMatricula: data })
+                    
+                    console.log(alunosDaEscola[i])
 
+                }
+
+              
+                   //return alunosDaEscola[prop].nome
+                   
+                }
+               
+            }
+           
+        
+
+          
+        
  adicionarAluno("Rafael") 
  console.log("###Alunos Cadastrados###")  
  listarAlunos()
- buscarAluno("Rafael")
+ buscarAluno("Rafael22")
+ matricularAluno("Rafael", 'Full Stack');
+
