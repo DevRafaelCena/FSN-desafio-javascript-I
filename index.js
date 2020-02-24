3a75a2b1a8dce46fb23d4713360ba989dddaab50
DevRafaelCena <rafael.cena@hotmail.com>
2020-02-24 11:19:30 -0300

    funções add,buscar,listar

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson
 
 
 // implementação
+
+function adicionarAluno(nome){
+
+    alunosDaEscola.push({nome})
+
+    return("Sucesso ao adcionar Aluno")
+  
+    /*Essa função irá receber uma *string* que é nome do aluno a ser criado. 
+    E seguindo o modelo de aluno, o mesmo deverá ser inserido na lista de alunos.
+    A função deve devolver um feedback de sucesso, caso o aluno seja inserido corretamente.*/
+    }
+    function listarAlunos(){
+
+      
+      for (variavel of alunosDaEscola) {
+        console.log(variavel.nome)
+        
+      }
+        }
+        function buscarAluno(nome){
+            /* Por meio dessa função, podemos pesquisar um aluno por nome na lista de aluno. Ela deverá exibir um feedback, tanto para quando encontrar o aluno, tanto quando não encontrar. E deverá devolver um aluno em seu retorno. */
+            const busca = alunosDaEscola.filter(aluno => aluno.nome == nome)
+
+            if(busca.length < 1){
+                console.log( "Aluno não encontrado!!!")
+            } else { console.log("Aluno encontrado!!!") 
+            return busca}
+           
+
+        }     
+
+
+ adicionarAluno("Rafael") 
+ console.log("###Alunos Cadastrados###")  
+ listarAlunos()
+ buscarAluno("Rafael")
